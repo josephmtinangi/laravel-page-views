1e6fc456052cfebe727b75a87ffa7fea4278d056
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-13 13:55:55 +0300

    Create Article model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Article::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'user_id' => function () {
+        	return factory('App\User')->create()->id;
+        },
+    ];
+});
