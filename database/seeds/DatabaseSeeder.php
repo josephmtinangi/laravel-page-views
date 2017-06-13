f94ad8b96600d190563044567149d824c8aa4c5d
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-13 18:22:11 +0300

    Create seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $users = factory('App\User', 5)->create();
+
+        $articles = $users->each(function ($user) {
+            factory('App\Article', 3)->create(['user_id' => $user->id]);
+        });
+
+        $articles->each(function ($article) {
+            factory('App\Comment', 2)->create(['article_id' => $article->id]);
+        });
     }
 }
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -13,13 +13,9 @@
                         </div>
                         <div class="col-md-8 biography-into">
                             <h4>{{ $author->name }}</h4>
-                            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below ook It has
-                                survived not only five centuries, a galley of type and reproduced scrambled survived not
-                                only five it to make a type specimen book for those interested. Sections 1.10.32 and
-                                1.10.33
-                                from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact
-                                original
-                                form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                            <p>
+                                {{ $author->bio }}
+                            </p>
                             <a href="details.html" class="link">Read More</a>
                         </div>
                         <div class="clearfix"></div>
